d02ee54174962d943a0c9d8f09856c8a4fed89e5
kongzZ <kongzz45@foxmail.com>
2022-08-25 00:02:32 +0800

    public

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,52 @@
 module.exports = {
-    base: "/blogs/",
-    head: [
-        ['link', {
-            rel: 'icon',
-            href: 'walle.png'
-        }]
+  base: "/blogs/",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "walle.png",
+      },
     ],
-    title: '达那蛋',
-    description: '达那蛋',
-    theme: '@vuepress/theme-default',
-    themeConfig: {
-        sidebar: 'auto',
-        lastUpdated: 'Last Updated',
-        smoothScroll: true,
-        nextLinks: true,
-        prevLinks: true,
-        navbar: [{
-                text: '首页',
-                link: '/'
-            },
-            {
-                text: '运维',
-                link: '/devops/'
-            },
-            {
-                text: '日报',
-                link: '/daily/'
-            },
-            {
-                text: '算法',
-                link: '/algorithm/'
-            },
-            {
-                text: '监控',
-                link: '/monitor/'
-            },
-            {
-                text: '基础',
-                link: '/basic/'
-            },
-        ]
-    }
-}
+  ],
+  title: "OMG运维管理",
+  description: "达那蛋",
+  theme: "@vuepress/theme-default",
+  themeConfig: {
+    sidebar: "auto",
+    // lastUpdated: "Last Updated",
+    smoothScroll: true,
+    nextLinks: true,
+    prevLinks: true,
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      //   {
+      //     text: "运维",
+      //     link: "/devops/",
+      //   },
+      {
+        text: "日报",
+        link: "/daily/",
+      },
+      //   {
+      //     text: "算法",
+      //     link: "/algorithm/",
+      //   },
+      {
+        text: "监控",
+        link: "/monitor/",
+      },
+      {
+        text: "容器",
+        link: "/container/",
+      },
+      //   {
+      //     text: "基础",
+      //     link: "/basic/",
+      //   },
+    ],
+  },
+};
