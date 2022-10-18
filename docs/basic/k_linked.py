fb74ffcdce86bbcc2f36f21effcf341b58987832
kongzZ <kongzz45@foxmail.com>
2022-10-18 23:14:49 +0800

    public

---
+++ docs/basic/k_linked.py
@@ -0,0 +1,42 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int):
+        start = head
+        end = self.get_group_end(start, k)
+        if end is None:
+            return head
+        head = end
+
+        self.reverse(start, end)
+        last_end = start
+        while last_end.next != None:
+            start = last_end.next
+            end = self.get_group_end(start, k)
+            if end is None:
+                return head
+            self.reverse(start, end)
+            last_end.next = end 
+            last_end = start
+        return head
+
+    def reverse(self, start: ListNode, end: ListNode):
+        end = end.next
+        pre = None
+        cur = start
+        next = None
+        while cur != end:
+            next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+        start.next = end
+
+    def get_group_end(self, start: ListNode, k: int):
+        while k != 0 and start:
+            start = start.next
+            k = k - 1
+        return start
---
+++ docs/basic/linked_list.md
@@ -0,0 +1,9 @@
+# 链表
+
+[25. K 个一组翻转链表](https://leetcode.cn/problems/reverse-nodes-in-k-group/)
+
+@[code](./k_linked.py)
+
+[面试题 02.05. 链表求和](https://leetcode.cn/problems/sum-lists-lcci/)
+
+[剑指 Offer 25. 合并两个排序的链表](https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/)
