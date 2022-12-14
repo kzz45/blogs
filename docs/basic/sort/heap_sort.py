504d6000cdb25fd3ee2e95baf9715f2ecbfa3456
kongzZ <kongzz45@foxmail.com>
2022-12-14 17:49:45 +0800

    public

--- docs/basic/sort/heap_sort.py
+++ docs/basic/sort/heap_sort.py
@@ -1,17 +1,28 @@
-def swap(arr, l, r):
-    arr[l], arr[r] = arr[r], arr[l]
+def heapify(arr, n, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
 
 
-def heap(arr, index):
-    while True:
-        if arr[index] > arr[(index - 1) // 2]:
-            swap(arr, index, (index - 1) // 2)
-            index = (index - 1) // 2
-        else:
-            break
+def heap_sort(arr):
+    n = len(arr)
+    # 构建大根堆
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
 
 
-arr = [2, 3, 6, 8, 1, 9]
-print(arr)
-heap(arr, 2)
+arr = [10, 7, 8, 1, 5, 3, 2, 7, 6]
+heap_sort(arr)
 print(arr)
