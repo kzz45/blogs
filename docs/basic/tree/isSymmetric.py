f5750dccf52b6aa02d8f59605ebe44f88b375971
kongzZ <kongzz45@foxmail.com>
2022-12-21 18:24:28 +0800

    public

---
+++ docs/basic/tree/isSymmetric.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.compare(root.left, root.right)
+
+    def compare(self, left, right):
+        if left is None and right is not None:
+            return False
+        elif left is not None and right is None:
+            return False
+        elif left is None and right is None:
+            return True
+        elif left.val != right.val:
+            return False
+
+        outside = self.compare(left.left, right.right)
+        inside = self.compare(left.right, right.left)
+        return outside and inside
