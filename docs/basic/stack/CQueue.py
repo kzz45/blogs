c283e2d18407631940c3496ee101de5c0a0bac26
kongzZ <kongzz45@foxmail.com>
2022-12-30 18:42:51 +0800

    public

--- docs/basic/stack/CQueue.py
+++ docs/basic/stack/CQueue.py
@@ -1,13 +1,22 @@
 class CQueue:
 
     def __init__(self):
-        pass
+        self.instack = []
+        self.outstack = []
 
     def appendTail(self, value: int) -> None:
-        pass
+        self.instack.append(value)
 
     def deleteHead(self) -> int:
-        pass
+        if len(self.outstack) == 0 and len(self.instack) == 0:
+            return -1
+        if len(self.outstack) == 0:
+            while len(self.instack) != 0:
+                self.outstack.append(self.instack[-1])
+                self.instack.pop()
+        top = self.outstack[-1]
+        self.outstack.pop()
+        return top
 
 
 # Your CQueue object will be instantiated and called as such:
